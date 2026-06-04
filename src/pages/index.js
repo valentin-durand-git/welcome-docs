85cbe4572c91a0f7053baa1a692f7684f7bddcf6
vdurandmail-design <v.durand.mail@gmail.com>
2026-06-04 12:33:53 +0200

    pRevert "feat: clean homepage"
    This reverts commit 093c85f178812e4d92dbca8ba4c85b1503955c29.

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,8 @@ import clsx from 'clsx';
 import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
+import HomepageFeatures from '@site/src/components/HomepageFeatures';
+
 import Heading from '@theme/Heading';
 import styles from './index.module.css';
 
@@ -17,8 +19,8 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/architecture/intro">
-            Accéder à la documentation →
+            to="/docs/intro">
+            Docusaurus Tutorial - 5min ⏱️
           </Link>
         </div>
       </div>
@@ -30,8 +32,12 @@ export default function Home() {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      title={siteConfig.title}
-      description="Documentation technique">
+      title={`Hello from ${siteConfig.title}`}
+      description="Description will go into a meta tag in <head />">
       <HomepageHeader />
+      <main>
+        <HomepageFeatures />
+      </main>
     </Layout>
   );
+}
